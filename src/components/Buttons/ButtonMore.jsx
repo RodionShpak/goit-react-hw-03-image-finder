df6c7ee859da8d120e21a7d5fb8bbd7012d07e95
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-10-13 11:12:14 +0300

    load

--- src/components/Buttons/ButtonMore.jsx
+++ src/components/Buttons/ButtonMore.jsx
@@ -4,7 +4,7 @@ export const ButtonMore = ({ onClick }) => {
   return (
     <>
       <Box display="flex" justifyContent="center" mb={5}>
-        <SC.ButtonMore onClick={onClick}>more</SC.ButtonMore>
+        <SC.ButtonMore onClick={onClick}>Load more</SC.ButtonMore>
       </Box>
     </>
   );
