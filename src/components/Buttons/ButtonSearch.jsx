23218a07e390da5639340e22db89d4f5d710775a
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-10-13 20:27:07 +0300

    ci

--- src/components/Buttons/ButtonSearch.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonS } from "./ButtonSearch.styled";
-import { AiOutlineSearch } from "react-icons/ai";
-export const ButtonSearch = () => (
-  <>
-    <ButtonS type="submit">
-      <AiOutlineSearch size="24px"></AiOutlineSearch>
-    </ButtonS>
-  </>
-);
