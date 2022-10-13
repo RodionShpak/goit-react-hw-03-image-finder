cd5cbf8373560c5421780d6bb3e5ead0013e7f1b
Rodion Shpak <98263512+RodionShpak@users.noreply.github.com>
2022-10-13 11:39:47 +0300

    g

--- src/components/Buttons/ButtonMore.jsx
+++ src/components/Buttons/ButtonMore.jsx
@@ -4,13 +4,8 @@ export const ButtonMore = ({ onClick }) => {
   return (
     <>
       <Box display="flex" justifyContent="center" mb={5}>
-        <SC.ButtonMore onClick={onClick}>Load more</SC.ButtonMore>
+        <SC.ButtonMore onClick={onClick}>more</SC.ButtonMore>
       </Box>
     </>
   );
 };
-
-
-
-
-
